3b3c44c18a85396ae1161e28efbbf7da10b81068
tsv2013 <tsv2013>
2015-08-22 12:15:29 +0300

    Fix gh-pages deploy script

--- deploy-ghpages.cmd
+++ deploy-ghpages.cmd
@@ -2,14 +2,13 @@ rmdir out /s /y
 
 md out
 cd out
-md dist
+
+git clone "https://github.com/tsv2013/algorithm.git" .
+git pull "https://github.com/tsv2013/algorithm.git" gh-pages
+git checkout gh-pages
+
 copy ..\dist\*.* .\dist\*.*
 
-
-git init
-git config user.name "tsv2013"
-rem git config user.email "noreply@home.org"
-
-git add .\dist\*.*
+git add .
 git commit -m "Deployed to Github Pages"
-git push --force "https://github.com/tsv2013/algorithm.git" master:gh-pages
+git push "https://github.com/tsv2013/algorithm.git" master:gh-pages

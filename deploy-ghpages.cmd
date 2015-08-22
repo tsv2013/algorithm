0675396db01d257e2a07ea9f26ecdecc0a24591d
tsv2013 <tsv2013>
2015-08-22 12:47:38 +0300

    Gh-pages deployment script does not work

--- deploy-ghpages.cmd
+++ deploy-ghpages.cmd
@@ -4,11 +4,10 @@ md out
 cd out
 
 git clone "https://github.com/tsv2013/algorithm.git" .
-git pull "https://github.com/tsv2013/algorithm.git" gh-pages
 git checkout gh-pages
 
 copy ..\dist\*.* .\dist\*.*
 
-git add .
-git commit -m "Deployed to Github Pages"
-git push "https://github.com/tsv2013/algorithm.git" master:gh-pages
+rem git add .
+rem git commit -m "Deployed to Github Pages"
+rem git push "https://github.com/tsv2013/algorithm.git" master:gh-pages
